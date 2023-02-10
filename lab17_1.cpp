175c3bcd4e56a2c4b73781274ae3d1fd3fa374d6
JayAleecha <aleecha.2546@gmail.com>
2023-02-10 17:32:53 +0700

    lab17_1

--- lab17_1.cpp
+++ lab17_1.cpp
@@ -32,3 +32,40 @@ int main(){
 	findColSum(dPtr,sum2,N,M); 
 	showData(sum2,1,M);
 }
+void showData(double a[],int N,int M){
+    cout << fixed << setprecision(2);
+    for(int i = 0; i < N*M; i++){
+        cout << a[i];
+        if((i+1)%M==0) cout << endl;
+        else cout << " ";
+    }
+}
+void randData(double a[],int N,int M){
+    for(int i = 0; i < N*M; i++){
+        a[i] = (rand()%101)/100.00;
+    }
+}
+void findRowSum(const double *a,double *b,int N,int M){
+    int s=0;
+    double z=0;
+    for(int i = 0; i < N*M; i++){
+        z+=*(a+i);
+        if((i+1)%M==0){
+            *(b+s)=z;
+            s++;
+            z=0;
+        }
+    }
+}
+void findColSum(const double *a,double *b,int N,int M){
+    int s=0;
+    double z=0;
+    for(int i = 0; i < N*M; i++){
+        z+=*(a+s+((i%N)*M));
+        if((i+1)%N==0){
+            *(b+s)=z;
+            s++;
+            z=0;
+        }
+    }
+}
